1ca0baaa441dee182f6926cc2b312f04e12e7499
UkrNicolasCage <vlaluk352@gmail.com>
2022-09-19 16:35:40 +0300

    default setting

---
+++ src/types/react-app-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react-scripts" />
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.png";
